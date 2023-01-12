d261eba3f8b6755ed4442ecf91bb5e6377d12629
hyebeen byun <hyebeen2658@naver.com>
2023-01-12 12:24:31 +0900

    유틸 함수 만들기

--- client/lib/dom/getNode.js
+++ client/lib/dom/getNode.js
@@ -1,6 +1,6 @@
 function getNode(node) {
   if (typeof node !== "string") {
-    throw new Error("getNode의 함수의 인자는 문자 타입이어야 합니다.");
+    throw new TypeError("getNode의 함수의 인자는 문자 타입이어야 합니다.");
   }
 
   return document.querySelector(node);
@@ -8,7 +8,7 @@ function getNode(node) {
 
 function getNodes(node) {
   if (typeof node !== "string") {
-    throw new Error("getNode의 함수의 인자는 문자 타입이어야 합니다.");
+    throw new TypeError("getNode의 함수의 인자는 문자 타입이어야 합니다.");
   }
 
   return document.querySelectorAll(node);
