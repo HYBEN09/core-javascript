50fed0b8e694632360ed1ed31cdf97b6dbdb8c58
hyebeen byun <hyebeen2658@naver.com>
2023-01-16 16:21:34 +0900

    [추가] 초기화 시키기 - memo 유틸함수 추가, 적용

--- client/main.js
+++ client/main.js
@@ -9,26 +9,10 @@ import {
   getNodes,
   insertLast,
   invisibleElement,
+  memo,
   visibleElement,
 } from "./lib/index.js";
 
-//* [ 주사위 굴리기 ]
-// 1. dice 애니메이션 불러오기
-// 2. bindEvent 유틸함수 만들기
-// 3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
-// 4. 변수 보호를 위한 클로저 + IIFE 사용하기
-
-//* [ 레코드 리스트 보이기 ]
-// 1. handleRecord 함수를 만들기
-// 2. disable 활성 유틸 함수 만들기
-// 3. handleReset 함수를 만듭니다.
-// 4. visible 활성 유틸 함수 만들기
-// 5. toggleState 유틸 함수 만들기
-
-// * [ 레코드 템플릿 뿌리기 ]
-// 1. renderRecordListItem 함수 만들기
-// 2. HTML 템플릿 만들기
-// 3. 템플릿 뿌리기
 //*----------------------------------------------------------------------------
 
 // const rollingDiceButton = getNode(".buttonGroup > button:nth-child(1)");
@@ -46,9 +30,12 @@ let total = 0;
 
 const recordListWrapper = getNode(".recordListWrapper");
 
+memo("@tbody", () => getNode(".recordListWrapper tbody"));
+memo("@cube", () => getNode("#cube"));
+
 function renderRecordListItem() {
   // let diceValue = Number(cube.dataset.dice);
-  let diceValue = Number(attr("#cube", "data-dice"));
+  let diceValue = Number(attr(memo("@cube"), "data-dice"));
 
   const newRecordListItem = /* html */ `
   <tr>
@@ -58,7 +45,7 @@ function renderRecordListItem() {
   </tr>
   `;
 
-  insertLast(".recordListWrapper tbody", newRecordListItem);
+  insertLast(memo("@tbody"), newRecordListItem);
 
   recordListWrapper.scrollTop = recordListWrapper.scrollHeight;
 }
@@ -100,7 +87,7 @@ const handleRecord = () => {
 //* 주사위 초기화 함수
 const handleReset = () => {
   invisibleElement(recordListWrapper);
-  clearContents(".recordListWrapper tbody");
+  clearContents(memo("@tbody"));
 
   count = 0;
   total = 0;
