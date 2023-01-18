3837b069f237cd001cc97473ea0f8448a60c09c3
hyebeen byun <hyebeen2658@naver.com>
2023-01-18 16:24:13 +0900

    [ 추가 ] 재사용가능 함수 테스트

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
-import { insertLast, xhrData } from "./lib/index.js";
+import { delayP, insertLast, tiger, xhrData, xhrPromise } from "./lib/index.js";
 
+//* xhr
 // xhrData.get(
 //   "https://jsonplaceholder.typicode.com/users/1",
 //   (res) => {
@@ -10,3 +11,26 @@ import { insertLast, xhrData } from "./lib/index.js";
 //     insertLast("body", "데이터 로딩에 실패했습니다");
 //   }
 // );
+
+//* promise
+// xhrPromise
+//   .get("https://jsonplaceholder.typicode.com/users/1")
+//   .then((res) => {
+//     insertLast(document.body, JSON.stringify(res));
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//*fetch
+async function render() {
+  await delayP(2000);
+  let response = await tiger.get(
+    "https://jsonplaceholder.typicode.com/users/1"
+  );
+
+  // insertLast(document.body, JSON.stringify(response.data));
+  console.log(response.data);
+}
+
+render();
